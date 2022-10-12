237a08c795321acdbda5590928585f5daf8e4cf4
fhrsak <hrsak.filip@gmail.com>
2022-10-12 16:39:48 +0200

    Refactored Service test.

--- src/main/java/hr/hrsak/countries/controllers/CountriesController.java
+++ src/main/java/hr/hrsak/countries/controllers/CountriesController.java
@@ -1,6 +1,5 @@
 package hr.hrsak.countries.controllers;
 
-import hr.hrsak.countries.dto.Country;
 import hr.hrsak.countries.services.CountryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1")
@@ -18,7 +16,7 @@ public class CountriesController {
     private CountryService countryService;
 
     @GetMapping("/countries")
-    public ResponseEntity<List<Country>> getAllCountries() {
+    public ResponseEntity<String[]> getAllCountries() {
         return ResponseEntity.ok(countryService.getAllCountries());
     }
 }
--- src/main/java/hr/hrsak/countries/services/CountryService.java
+++ src/main/java/hr/hrsak/countries/services/CountryService.java
@@ -1,10 +1,6 @@
 package hr.hrsak.countries.services;
 
-import hr.hrsak.countries.dto.Country;
-
-import java.util.List;
-
 public interface CountryService {
 
-    List<Country> getAllCountries();
+    String[] getAllCountries();
 }
